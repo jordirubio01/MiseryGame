0d5701005f0790e7efdab3e98443996c218ab825
Jordi Rubio Arbona <131033900+jordirubio01@users.noreply.github.com>
2026-02-12 16:59:12 +0100

    2nd Commit
    - Basic GameManager created.
    - All needed scenes created.
    - Change between scenes included.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadDialogue()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
